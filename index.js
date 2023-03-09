80df6ee30fd44c26d7231b376752bd75f4cb9e2c
Thibaut <najarthibaut@gmail.com>
2023-03-09 17:35:30 +0100

    deuxième save pour verifier que je sais faire

--- index.js
+++ index.js
@@ -19,3 +19,9 @@ console.log(somme)
 const tabfruit = ['pomme','fraise','mangue','framboise','orange']
 tabfruit.push('citron')
 console.log(tabfruit)
+
+const tabnote = [15,5,8,9,16]
+let somme = tabnote
+for (let i = 1; i < tabnote.length; i++){
+   somme = somme + tabnote[i]
+}
