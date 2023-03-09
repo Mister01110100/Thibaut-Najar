a7179f890a2099d5ba675db479d366683f11aa1e
Thibaut <najarthibaut@gmail.com>
2023-03-09 17:09:41 +0100

    première save exo1

---
+++ index.js
@@ -0,0 +1,21 @@
+const tabchiffre = [1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i < tabchiffre.length; i++){
+    console.log(tabchiffre[i])
+ }
+
+const tabsemaine = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche']
+console.log(tabsemaine[0])
+
+const tabchiffrepair = [2,4,6,8,10]
+let somme = tabchiffrepair[0]
+console.log(somme)
+
+for (let i = 1; i < tabchiffrepair.length; i++){
+   somme = somme + tabchiffrepair[i]
+}
+console.log(somme)
+
+
+const tabfruit = ['pomme','fraise','mangue','framboise','orange']
+tabfruit.push('citron')
+console.log(tabfruit)
